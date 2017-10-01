8daeff640c34aff9ebca0baf6c834e0d62e65a0b
mrdulin <novaline.dulin@gmail.com>
2017-10-01 21:58:28 +0800

    add sample

---
+++ src/stackoverflow/37764363/index.js
@@ -0,0 +1,16 @@
+var request = require("request");
+
+function func(arg1, arg2) {
+  request.get("https://github.com/mrdulin", function(error, response, body) {
+    console.log(arg1, arg2);
+    if (error) {
+      console.log(error);
+    } else if (response.status === 200) {
+      console.log(response);
+    } else {
+      console.log("others");
+    }
+  });
+}
+
+exports.func = func;
---
+++ src/stackoverflow/37764363/index.spec.js
@@ -0,0 +1,53 @@
+var foo = require("./");
+var sinon = require("sinon");
+var request = require("request");
+
+describe("func", () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+  it("should handle error", () => {
+    const logSpy = sinon.spy(console, "log");
+    const getStub = sinon.stub(request, "get");
+    foo.func(1, 2);
+    const mError = new Error("network error");
+    getStub.yield(mError, null, null);
+    sinon.assert.calledWith(
+      getStub,
+      "https://github.com/mrdulin",
+      sinon.match.func
+    );
+    sinon.assert.calledWith(logSpy.firstCall, 1, 2);
+    sinon.assert.calledWith(logSpy.secondCall, mError);
+  });
+
+  it("should handle response", () => {
+    const logSpy = sinon.spy(console, "log");
+    const getStub = sinon.stub(request, "get");
+    foo.func(1, 2);
+    const mResponse = { status: 200 };
+    getStub.yield(null, mResponse, null);
+    sinon.assert.calledWith(
+      getStub,
+      "https://github.com/mrdulin",
+      sinon.match.func
+    );
+    sinon.assert.calledWith(logSpy.firstCall, 1, 2);
+    sinon.assert.calledWith(logSpy.secondCall, mResponse);
+  });
+
+  it("should handle other situation", () => {
+    const logSpy = sinon.spy(console, "log");
+    const getStub = sinon.stub(request, "get");
+    foo.func(1, 2);
+    const mResponse = { status: 500 };
+    getStub.yield(null, mResponse, null);
+    sinon.assert.calledWith(
+      getStub,
+      "https://github.com/mrdulin",
+      sinon.match.func
+    );
+    sinon.assert.calledWith(logSpy.firstCall, 1, 2);
+    sinon.assert.calledWith(logSpy.secondCall, "others");
+  });
+});
