bdd6d3a1ccfd08733368cd0a1176dd8b41368586
mrdulin <novaline.dulin@gmail.com>
2019-11-15 20:31:59 +0800

    add sample

---
+++ src/stackoverflow/58058653/submitDetails.spec.ts
@@ -0,0 +1,31 @@
+import { expect } from 'chai';
+import sinon, { SinonSandbox, SinonSpy } from 'sinon';
+
+const submitDetails = require('./submitDetails');
+const sendEmail = require('./sendEmail');
+
+describe('submitDetails', () => {
+  let sandbox: SinonSandbox;
+
+  before(() => {
+    sandbox = sinon.createSandbox();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('submitDetails', () => {
+    let sendEmailSpy: SinonSpy;
+
+    beforeEach(() => {
+      sendEmailSpy = sandbox.spy(sendEmail, 'sendEmail');
+    });
+
+    it('sendEmail', () => {
+      submitDetails.submitDetails();
+      sandbox.assert.calledOnce(sendEmailSpy);
+      expect(sendEmailSpy.calledOnce).to.be.true;
+    });
+  });
+});
---
+++ src/stackoverflow/58058653/submitDetails.ts
@@ -0,0 +1,10 @@
+const sendEmail = require('./sendEmail');
+// @ts-ignore
+const submitDetails = {};
+
+// @ts-ignore
+submitDetails.submitDetails = query => {
+  return sendEmail.sendEmail(query);
+};
+
+module.exports = submitDetails;
