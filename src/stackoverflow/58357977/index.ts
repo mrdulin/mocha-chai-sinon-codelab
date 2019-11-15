6223d7d020497921a8be0311d5515c3a57b12091
mrdulin <novaline.dulin@gmail.com>
2019-11-15 20:09:07 +0800

    add sample

---
+++ src/stackoverflow/58357977/index.ts
@@ -0,0 +1,20 @@
+export const logger = {
+  warn(message) {
+    console.warn(message);
+  }
+};
+
+export const yaml = {
+  safeLoad(file, callback) {}
+};
+
+export const fsExtra = {
+  readFileSync(filepath, options) {}
+};
+
+export function main() {
+  const filepath = './.tmp/sinon.js';
+  let file = yaml.safeLoad(fsExtra.readFileSync(filepath, 'utf8'), err => {
+    logger.warn(err);
+  });
+}
