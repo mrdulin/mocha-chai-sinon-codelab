3e62e7451bd55083f261fc5c267c09cab35f8f9b
mrdulin <novaline.dulin@gmail.com>
2019-11-15 17:09:50 +0800

    add sample

---
+++ src/stackoverflow/58822996/index.ts
@@ -0,0 +1,13 @@
+import request from 'request';
+
+export function myRequest(targetUrl, reqBody) {
+  return new Promise((resolve, reject) => {
+    request.post(targetUrl, { json: reqBody }, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        resolve(body.transferId);
+      } else {
+        reject(error || body.description || body);
+      }
+    });
+  });
+}
