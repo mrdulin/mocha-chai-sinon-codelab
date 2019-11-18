98bbc89920c38983275a82951f1c634f89ffd739
mrdulin <novaline.dulin@gmail.com>
2019-11-18 17:33:49 +0800

    remove it.only

--- src/stackoverflow/57997682/index.spec.ts
+++ src/stackoverflow/57997682/index.spec.ts
@@ -3,15 +3,17 @@ import sinon from 'sinon';
 
 const Aupdate = require('./');
 
-it.only('should check if function is called or not', function() {
-  let reqUpdateAccount = {
-    body: {
-      Account: 'newAccount1'
-    }
-  };
-  const res = {};
-  let spy = sinon.spy(Aupdate, 'getUpdateArgs');
-  Aupdate.accountsUpdate(reqUpdateAccount, res);
-  expect(spy.calledWith({ Account: 'newAccount1' })).to.be.true;
-  sinon.assert.calledOnce(spy);
+describe('Aupdate', () => {
+  it('should check if function is called or not', function() {
+    let reqUpdateAccount = {
+      body: {
+        Account: 'newAccount1'
+      }
+    };
+    const res = {};
+    let spy = sinon.spy(Aupdate, 'getUpdateArgs');
+    Aupdate.accountsUpdate(reqUpdateAccount, res);
+    expect(spy.calledWith({ Account: 'newAccount1' })).to.be.true;
+    sinon.assert.calledOnce(spy);
+  });
 });
