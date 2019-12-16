229ef71216c612c9b2ae7f5f1df059c345640425
mrdulin <novaline.dulin@gmail.com>
2019-12-16 17:18:18 +0800

    improve sample

--- src/stackoverflow/59303752/files.js
+++ src/stackoverflow/59303752/files.js
@@ -1,12 +1,17 @@
+const fs = require("fs");
+
 class Files {
   constructor(queueNumber = 0) {
     this.queueNumber = queueNumber;
+    console.log("before: ", this.dir);
     this.dir = "JiraResults";
-  }
-
-  mkdir() {
-    console.log("make dir: ", this.dir);
+    console.log("after: ", this.dir);
+    if (!fs.existsSync(this.dir)) {
+      fs.mkdirSync(this.dir);
+    }
   }
 }
+
+Files.prototype.dir = "";
 
 module.exports = Files;
--- src/stackoverflow/59303752/files.test.js
+++ src/stackoverflow/59303752/files.test.js
@@ -1,12 +1,15 @@
 const Files = require("./files");
 const sinon = require("sinon");
+const fs = require("fs");
 
-describe("Files", () => {
-  it("should use stubbed dir", () => {
-    sinon.spy(console, "log");
-    const instance = new Files();
-    instance.dir = "stubbed dir";
-    instance.mkdir();
-    sinon.assert.calledWith(console.log, "make dir: ", "stubbed dir");
+describe.skip("Files", () => {
+  it("should use stubbed dir to mkdir", () => {
+    sinon.stub(fs, "existsSync").returns(false);
+    sinon.stub(fs, "mkdirSync");
+    sinon.stub(Files.prototype, "dir").value("stubbed dir");
+    console.log("stub dir");
+    new Files();
+    sinon.assert.calledWith(fs.existsSync, "stubbed dir");
+    sinon.assert.calledWith(fs.mkdirSync, "stubbed dir");
   });
 });
